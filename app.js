1ed92e9ef2e605c536dad67b5b5d2f09de7ee15b
Nazinur <nazinuramanova80@gmail.com>
2024-05-13 09:50:24 +0600

    first commit

---
+++ app.js
@@ -0,0 +1,14 @@
+//DOM - document object model
+
+
+const up=window.document.getelementbyld('up')
+const dowm=document.querySelector('#down')
+const reset=document.querySelector('.reset')
+const counter=document.querySelector('h1')
+
+// counter.innerHTML='<i>Ali</i>'
+// counter.innerText='<i>Ermek</i>'
+
+down.onclick=()=>{
+    
+}
